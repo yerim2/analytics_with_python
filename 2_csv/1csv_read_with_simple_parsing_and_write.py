b6664d3a3750c35824e659e17da10d6436d64192
anohk <anyoung.noh@gmail.com>
2018-01-14 14:25:29 +0900

    edit

--- 2_csv/1csv_read_with_simple_parsing_and_write.py
+++ 2_csv/1csv_read_with_simple_parsing_and_write.py
@@ -3,14 +3,12 @@ import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-
 with open(input_file, 'r', newline='') as filereader:
     with open(output_file, 'w', newline='') as filewriter:
         header = filereader.readline()
         header_list = header.strip().split(',')
-        filewriter.write(','.join(map(str, header_list))+'\n')
+        filewriter.write(','.join(map(str, header_list)) + '\n')
         for row in filereader:
             row = row.strip()
             row_list = row.split(',')
-            print(row_list)
-            filewriter.write(','.join(map(str, row_list))+'\n')
+            filewriter.write(','.join(map(str, row_list)) + '\n')
--- 2_csv/pandas_parsing_and_write.py
+++ 2_csv/pandas_parsing_and_write.py
@@ -5,5 +5,4 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 data_frame = pd.read_csv(input_file)
-print(data_frame)
 data_frame.to_csv(output_file, index=False)
